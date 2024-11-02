90d1fa93cdc98f299c8653b3f59886d28f416b79
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-03 01:03:24 +0700

    [ADD] add register route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import axios from "axios";
+import { LoginRequest } from "../models/user.model";
 
 const USER_SERVICE_URL = 'http://localhost:3773/api/v1/'
 
-
+// [TODO] need to handle Cookies
 export const login = async(req : Request , res : Response) =>{
     try {
-      const { email, password } = req.body;
+      const { email, password } : LoginRequest = req.body;
 
       if (!email || !password) {
           res.status(400).json({ error: "Email and password are required" });
@@ -19,9 +20,49 @@ export const login = async(req : Request , res : Response) =>{
                 password
             }
         );
-        res.json(response.data);
+        res.json({
+          success : true,
+          token : response.data 
+        });
+
       } catch (error) {
         console.log(error)
         res.status(500).json({ error: `login with USER_SERVICE error` });
       }
 }
+// [TODO] need to handle Cookies
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { 
+      username, 
+      email, 
+      password, 
+      firstName, 
+      lastName, 
+      phoneNumber, 
+      roomNumber 
+    } = req.body;
+
+    if (!email || !password || !username || !firstName || !lastName) {
+      res.status(400).json({ error: "Required fields are missing" });
+      return;
+    }
+
+    const response = await axios.post(`${USER_SERVICE_URL}register`, {
+      username,
+      email,
+      password,
+      firstName,
+      lastName,
+      phoneNumber,
+      roomNumber
+    });
+
+    res.json({
+      success: true,
+      token: response.data
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Register with USER_SERVICE error" });
+  }
+};
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { login } from "../controllers/user.controller";
+import { login, register } from "../controllers/user.controller";
 
 const router = Router()
 
 router.post('/login',login)
+router.post('/register',register)
 
 export default router
