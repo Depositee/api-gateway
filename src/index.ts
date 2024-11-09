8c846c548a19578b275a7195cd0205a5b62678d6
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-09 20:08:32 +0700

    enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import cors from 'cors';
 import orderRoutes from './routes/order.route'
 import reviewRoutes from './routes/review.route'
 import userRoutes from './routes/user.route'
@@ -7,6 +7,12 @@ import { authMiddleware } from './middlewares/auth.middleware';
 
 const app = express();
 const PORT = 8081;
+
+app.use(cors({
+  origin: '*', // Replace with your allowed origin(s)
+  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+  credentials: true,
+}));
 
 app.use(express.json());
 
