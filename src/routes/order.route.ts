01670f94fb1206d3536159ea3ca544c33f36ff3b
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-10 21:44:22 +0700

    Update order.route.ts

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createOrder, deleteOrder, getOrders, getOrdersByDepositorId, getOrdersById, updateOrder } from "../controllers/order.controller";
+import { createOrder, deleteOrder, getOrders, getOrdersByDepositeeId, getOrdersByDepositorId, getOrdersById, updateOrder } from "../controllers/order.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router()
@@ -7,7 +7,7 @@ const router = Router()
 router.get('/' ,getOrders)
 router.get('/my/:orderId' ,authMiddleware,getOrdersById)
 router.get('/my',authMiddleware,getOrdersByDepositorId)
-router.get('/myaccept',authMiddleware,getOrdersByDepositorId)
+router.get('/accept',authMiddleware,getOrdersByDepositeeId)
 router.post('/',authMiddleware,createOrder)
 router.put('/:orderId',authMiddleware, updateOrder)
 router.delete('/:orderId',authMiddleware,deleteOrder)
